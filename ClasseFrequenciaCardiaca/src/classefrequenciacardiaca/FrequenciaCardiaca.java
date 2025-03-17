4078249e4e425ccb41946f6ef04225a7b0d32a9c
alexritzmann <alexritzmann@gmail.com>
2025-03-17 20:57:26 -0300

    Adicionado atividade FrequenciaCardiaca

---
+++ ClasseFrequenciaCardiaca/src/classefrequenciacardiaca/FrequenciaCardiaca.java
@@ -0,0 +1,70 @@
+package classefrequenciacardiaca;
+
+import java.util.Calendar;
+
+
+public class FrequenciaCardiaca
+{
+
+    private int diaNascimento;
+    private int mesNascimento;
+    private int anoNascimento;
+    private String nome;
+    private String sobrenome;
+
+    public int getDiaNascimento() {return diaNascimento;}
+
+    public void setDiaNascimento(int diaNascimento) {this.diaNascimento = diaNascimento;}
+
+    public int getMesNascimento() {return mesNascimento;}
+
+    public void setMesNascimento(int mesNascimento) {this.mesNascimento = mesNascimento;}
+
+    public int getAnoNascimento() {return anoNascimento;}
+
+    public void setAnoNascimento(int anoNascimento) {this.anoNascimento = anoNascimento;}
+
+    public String getNome() {return nome;}
+
+    public void setNome(String nome) {this.nome = nome;}
+
+    public String getSobrenome() {return sobrenome;}
+
+    public void setSobrenome(String sobrenome) {this.sobrenome = sobrenome;}
+
+    public FrequenciaCardiaca(String nome, String sobrenome, int diaNascimento, int mesNascimento, int anoNascimento)
+    {
+        this.nome = nome;
+        this.sobrenome = sobrenome;
+        this.anoNascimento = anoNascimento;
+        this.mesNascimento = mesNascimento;
+        this.diaNascimento = diaNascimento;
+    }
+
+    public int calcularIdade(int dia, int mes, int ano)
+    {
+        Calendar hoje = Calendar.getInstance();
+        int diaAtual = hoje.get(Calendar.DAY_OF_MONTH);
+        int mesAtual = hoje.get(Calendar.MONTH) + 1;
+        int anoAtual = hoje.get(Calendar.YEAR);
+        int idade = anoAtual - ano;
+
+        if (mesAtual < mes || (mesAtual == mes && diaAtual < dia))
+        {
+            idade--;
+        }
+        return idade;
+    }
+
+    public void calculoFrequenciaCardiaca(int dia, int mes, int ano)
+    {
+        int idade = calcularIdade(dia, mes, ano);
+        int frequencia = 220 - idade;
+
+        System.out.println("Sua frequencia cardiaca máxima é de " + frequencia);
+
+        System.out.println("O alvo da sua frenquencia cardiaca é estar entre " +frequencia*0.5 + " e " + frequencia*0.85);
+    }
+
+
+}
---
+++ ClasseFrequenciaCardiaca/src/classefrequenciacardiaca/Main.java
@@ -0,0 +1,49 @@
+package classefrequenciacardiaca;
+
+import java.util.Scanner;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Scanner entrada = new Scanner(System.in);
+
+
+
+        System.out.println("Hoje vamos calcular sua frequencia cardíaca. Para isso preciso de algumas informações. ok?");
+
+        System.out.println("Primero, informe o ano de seu nascimento:");
+        int ano = entrada.nextInt();
+
+        System.out.println("Agora digite o mês de seu nascimento:");
+        int mes = entrada.nextInt();
+
+        System.out.println("E agora o dia do seu nascimento:");
+        int dia = entrada.nextInt();
+        entrada.nextLine();
+
+        System.out.println("Qual o seu primeiro nome?");
+        String nome = entrada.nextLine();
+
+        System.out.println("Qual o seu sobrenome?");
+        String sobrenome = entrada.nextLine();
+
+
+        FrequenciaCardiaca usuario = new FrequenciaCardiaca(nome, sobrenome, dia,mes,ano);
+        usuario.setNome(nome);
+        usuario.setSobrenome(sobrenome);
+        usuario.setAnoNascimento(ano);
+        usuario.setMesNascimento(mes);
+        usuario.setDiaNascimento(dia);
+
+
+        System.out.println("Olá " + usuario.getNome() + " " + usuario.getSobrenome());
+        System.out.println("Sua data de nascimento é " +usuario.getDiaNascimento()+"/"+usuario.getMesNascimento()+"/"+usuario.getAnoNascimento());
+        System.out.println("Sua idade hoje é de " + usuario.calcularIdade(dia,mes,ano) + " anos");
+        usuario.calculoFrequenciaCardiaca(usuario.getDiaNascimento(),usuario.getMesNascimento(),usuario.getAnoNascimento());
+
+
+
+
+    }
+}
