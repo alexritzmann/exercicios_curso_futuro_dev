51e88acb394d6d8fda14099d94945595a1957e83
alexritzmann <alexritzmann@gmail.com>
2025-05-04 15:17:42 -0300

    M1S10 para M1S11 (definitivo)

--- Spring/M1S11/src/main/java/futurodevv1/m1s10/errors/exeptions/NotFoundException.java
+++
@@ -1,9 +0,0 @@
-package futurodevv1.m1s10.errors.exeptions;
-
-public abstract class NotFoundException extends RuntimeException
-{
-    public NotFoundException(String message)
-    {
-        super(message);
-    }
-}
