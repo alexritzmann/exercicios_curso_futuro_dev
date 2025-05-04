abb74c79b64dfc9167adc0eb1e1d3cc496c7aad2
Alex Ritzmann <alexritzmann@gmail.com>
2025-05-04 15:13:30 -0300

    Delete Spring/M1S11/src/main/java/futurodevv1/m1s10 directory
    clonado para m1s11

--- Spring/M1S11/src/main/java/futurodevv1/m1s10/dtos/OrganizationRequestDto.java
+++
@@ -1,7 +0,0 @@
-package futurodevv1.m1s10.dtos;
-
-public record OrganizationRequestDto(
-        String name,
-        String contact
-) {
-}
--- Spring/M1S11/src/main/java/futurodevv1/m1s10/repositories/OrganizationRepository.java
+++
@@ -1,8 +0,0 @@
-package futurodevv1.m1s10.repositories;
-
-import futurodevv1.m1s10.entities.Organization;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrganizationRepository extends JpaRepository<Organization, Long>
-{
-}
